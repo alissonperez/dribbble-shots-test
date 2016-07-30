f94ec53cd2438240b87a0749dbc621f0e43a1751
Alisson Perez <alissonperez@gmail.com>
2016-07-30 19:11:39 -0300

    Add local webserver to use with ajax calls

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var stylus = require('gulp-stylus');
+var webserver = require('gulp-webserver');
 
 
 var STYLUS_SRC = './styl/**/*.styl';
@@ -7,13 +8,23 @@ var STYLUS_SRC = './styl/**/*.styl';
 
 // Compile stylus files
 gulp.task('stylus', function () {
-    return gulp.src(STYLUS_SRC).pipe(stylus()).pipe(gulp.dest('./css/'));
+  return gulp.src(STYLUS_SRC).pipe(stylus()).pipe(gulp.dest('./css/'));
 });
 
 
 // Add watchers
 gulp.task('serve', ['stylus'], function() {
-    gulp.watch(STYLUS_SRC, ['stylus']);
+  gulp.watch(STYLUS_SRC, ['stylus']);
+});
+
+
+// Webserver
+gulp.task('webserver', function() {
+  gulp.src('.')
+    .pipe(webserver({
+      livereload: true,
+      open: true
+    }));
 });
 
 
