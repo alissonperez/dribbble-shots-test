c38561ddddb75fb4f5250fe0a9ac95e326c904ec
Alisson Perez <alissonperez@gmail.com>
2016-07-30 21:43:59 -0300

    Remove jquery, not used.

--- karma.conf.js
+++ karma.conf.js
@@ -17,7 +17,6 @@ module.exports = function(config) {
     files: [
       'node_modules/angular/angular.js',
       'node_modules/angular-mocks/angular-mocks.js',
-      'js/vendor/**/*.js',
       'js/*.js',
       'test/**/*Spec.js'
     ],
